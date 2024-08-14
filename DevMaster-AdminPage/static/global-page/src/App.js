c4effd31a2d02169c7d106e6866419053c3e7a85
guimelao94 <guimelao94@yahoo.es>
2024-08-14 18:59:39 -0400

    clean up

--- DevMaster-AdminPage/static/global-page/src/App.js
+++ DevMaster-AdminPage/static/global-page/src/App.js
@@ -1,17 +1,10 @@
 import React, { useEffect, useReducer, useState } from 'react';
-import { invoke } from '@forge/bridge';
 import BasicGrid from './Components/BasicGrid';
-import ResponsiveGrid from './Components/ResponsiveGrid';
-import { Stack } from '@atlaskit/primitives';
 import { ProductLayout } from './Components/ProductLayout';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import { EpicCard } from './Components/EpicCard/EpicCard';
-import { view, requestJira } from '@forge/bridge';
-import { GlobalContextProvider } from './context/GlobalContext';
 
 function App() {
-    const [, forceRender] = useReducer(x => x + 1, 0);
 
     return (
         <Provider store={store}>
--- DevMaster-AdminPage/static/global-page/src/hooks/useGlobal.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-const useGlobal = () => {
-    const [epics,setEpics] = useState({ Epic: "DMA-2" });
-  return {epics,setEpics}
-}
-
-export default useGlobal;
